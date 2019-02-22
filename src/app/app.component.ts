357f900227664fb9607c485c4cbd4522ca99ffda
mhosan <mhosan@gmail.com>
2019-02-22 18:43:23 -0300

    viernes...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,47 @@ import { JsonService} from './json.service';
 })
 export class AppComponent {
   title = 'Testeo';
+  poblacion = {
+    fechaHoy: <string> null,
+    cantidadHoy: <string> null,
+    fechaTomorrow: <string> null,
+    cantidadTomorrow: <string> null
+  };
+  poblacionArg = {
+    fechaHoy: <string> null,
+    cantidadHoy: <string> null,
+    fechaTomorrow: <string> null,
+    cantidadTomorrow: <string> null
+  };
+  poblacionBr = {
+    fechaHoy: <string> null,
+    cantidadHoy: <string> null,
+    fechaTomorrow: <string> null,
+    cantidadTomorrow: <string> null
+  };
+  poblacionCh = {
+    fechaHoy: <string> null,
+    cantidadHoy: <string> null,
+    fechaTomorrow: <string> null,
+    cantidadTomorrow: <string> null
+  };
+  poblacionBl = {
+    fechaHoy: <string> null,
+    cantidadHoy: <string> null,
+    fechaTomorrow: <string> null,
+    cantidadTomorrow: <string> null
+  };
+  poblacionPr = {
+    fechaHoy: <string> null,
+    cantidadHoy: <string> null,
+    fechaTomorrow: <string> null,
+    cantidadTomorrow: <string> null
+  };
   constructor(private json: JsonService){
   }
-
   ngOnInit(){
-    this.hacerAlgo();
+    // this.hacerAlgo();
   }
-
   hacerAlgo(){
     //let i = 100;
     //this.json.getData("").subscribe(respuesta => {
@@ -24,7 +58,72 @@ export class AppComponent {
       //console.log(respuesta.latt + ", " + respuesta.longt);
       //console.log(respuesta[i].title + ", " + respuesta[i].year + ", " + respuesta[i].director + ", " + respuesta[i].cast + ", " + respuesta[i].genre);
     });
-
+  }
+  poblacionMundial(event: any){
+    this.json.getData("http://api.population.io/1.0/population/World/today-and-tomorrow/?format=json").subscribe(respuesta => {
+      console.log("Respuesta:", respuesta);
+      this.poblacion = {
+        fechaHoy : respuesta.total_population[0].date,
+        cantidadHoy : respuesta.total_population[0].population,
+        fechaTomorrow: respuesta.total_population[1].date,
+        cantidadTomorrow: respuesta.total_population[1].population
+      };
+    });
+  }
+  poblacionArgentina(event: any){
+    this.json.getData("http://api.population.io/1.0/population/Argentina/today-and-tomorrow/?format=json").subscribe(respuesta => {
+      console.log("Respuesta:", respuesta);
+      this.poblacionArg = {
+        fechaHoy : respuesta.total_population[0].date,
+        cantidadHoy : respuesta.total_population[0].population,
+        fechaTomorrow: respuesta.total_population[1].date,
+        cantidadTomorrow: respuesta.total_population[1].population
+      };
+    });
+  }
+  poblacionBrasil(event: any){
+    this.json.getData("http://api.population.io/1.0/population/Brazil/today-and-tomorrow/?format=json").subscribe(respuesta => {
+      console.log("Respuesta:", respuesta);
+      this.poblacionBr = {
+        fechaHoy : respuesta.total_population[0].date,
+        cantidadHoy : respuesta.total_population[0].population,
+        fechaTomorrow: respuesta.total_population[1].date,
+        cantidadTomorrow: respuesta.total_population[1].population
+      };
+    });
+  }
+  poblacionChile(event: any){
+    this.json.getData("http://api.population.io/1.0/population/Chile/today-and-tomorrow/?format=json").subscribe(respuesta => {
+      console.log("Respuesta:", respuesta);
+      this.poblacionCh = {
+        fechaHoy : respuesta.total_population[0].date,
+        cantidadHoy : respuesta.total_population[0].population,
+        fechaTomorrow: respuesta.total_population[1].date,
+        cantidadTomorrow: respuesta.total_population[1].population
+      };
+    });
+  }
+  poblacionBolivia(event: any){
+    this.json.getData("http://api.population.io/1.0/population/Bolivia/today-and-tomorrow/?format=json").subscribe(respuesta => {
+      console.log("Respuesta:", respuesta);
+      this.poblacionBl = {
+        fechaHoy : respuesta.total_population[0].date,
+        cantidadHoy : respuesta.total_population[0].population,
+        fechaTomorrow: respuesta.total_population[1].date,
+        cantidadTomorrow: respuesta.total_population[1].population
+      };
+    });
+  }
+  poblacionParaguay(event: any){
+    this.json.getData("http://api.population.io/1.0/population/Paraguay/today-and-tomorrow/?format=json").subscribe(respuesta => {
+      console.log("Respuesta:", respuesta);
+      this.poblacionPr = {
+        fechaHoy : respuesta.total_population[0].date,
+        cantidadHoy : respuesta.total_population[0].population,
+        fechaTomorrow: respuesta.total_population[1].date,
+        cantidadTomorrow: respuesta.total_population[1].population
+      };
+    });
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+//
+//Este es el script javascript que se ejecuta para poner en marcha la aplicacion
+//Se mantiene que el html que se ejecuta para poner en marcha la aplicación es el index.html
+//
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -8,5 +13,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule)     //<---- Aqui se indica por donde va a comenzar la aplicación, o sea "AppModule"
+  .catch(err => console.log(err));                      //bootstrap provocará que Angular lea el codigo del modulo, sepa que componentes
+                                                        //existen para ponerlos en marcha.
