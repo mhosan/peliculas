f2b00366cbb023bb00a8b8fff9b5f7b190a94510
mhosan <mhosan@gmail.com>
2019-02-23 20:58:18 -0300

    hoy es sabado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from 'node_modules/@angular/core';
 import { JsonService} from './json.service';
+import { IPoblacion } from './poblacion.interface';
 
 @Component({
   selector: 'app-root',
@@ -8,150 +9,100 @@ import { JsonService} from './json.service';
 })
 export class AppComponent {
   title = 'Testeo';
-  poblacion = {
-    fechaHoy: <string> null,
-    cantidadHoy: <string> null,
-    fechaTomorrow: <string> null,
-    cantidadTomorrow: <string> null
-  };
-  poblacionArg = {
-    fechaHoy: <string> null,
-    cantidadHoy: <string> null,
-    fechaTomorrow: <string> null,
-    cantidadTomorrow: <string> null
-  };
-  poblacionBr = {
-    fechaHoy: <string> null,
-    cantidadHoy: <string> null,
-    fechaTomorrow: <string> null,
-    cantidadTomorrow: <string> null
-  };
-  poblacionCh = {
-    fechaHoy: <string> null,
-    cantidadHoy: <string> null,
-    fechaTomorrow: <string> null,
-    cantidadTomorrow: <string> null
-  };
-  poblacionBl = {
-    fechaHoy: <string> null,
-    cantidadHoy: <string> null,
-    fechaTomorrow: <string> null,
-    cantidadTomorrow: <string> null
-  };
-  poblacionPr = {
-    fechaHoy: <string> null,
-    cantidadHoy: <string> null,
-    fechaTomorrow: <string> null,
-    cantidadTomorrow: <string> null
-  };
-  poblacionUr = {
-    fechaHoy: <string> null,
-    cantidadHoy: <string> null,
-    fechaTomorrow: <string> null,
-    cantidadTomorrow: <string> null
-  };
-  poblacionPeru = {
-    fechaHoy: <string> null,
-    cantidadHoy: <string> null,
-    fechaTomorrow: <string> null,
-    cantidadTomorrow: <string> null
-  };
+  public pobMundial: IPoblacion = {} as IPoblacion;
+  public pobArgentina: IPoblacion = {} as IPoblacion;
+  public pobBrasil: IPoblacion = {} as IPoblacion;
+  public pobChile: IPoblacion = {} as IPoblacion;
+  public pobBolivia: IPoblacion = {} as IPoblacion;
+  public pobParaguay: IPoblacion = {} as IPoblacion;
+  public pobUruguay: IPoblacion = {} as IPoblacion;
+  public pobPeru: IPoblacion = {} as IPoblacion;
+
   constructor(private json: JsonService){
   }
-  ngOnInit(){
-    // this.hacerAlgo();
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.hacerAlgo();
   }
-  hacerAlgo(){
-    //let i = 100;
-    //this.json.getData("").subscribe(respuesta => {
-    this.json.getData("https://api.github.com/users/mhosan/repos").subscribe(respuesta => {
-      console.log("Respuesta:");
-      console.log(respuesta);
-      //console.log(respuesta.latt + ", " + respuesta.longt);
-      //console.log(respuesta[i].title + ", " + respuesta[i].year + ", " + respuesta[i].director + ", " + respuesta[i].cast + ", " + respuesta[i].genre);
-    });
-  }
-  poblacionMundial(event: any){
+  // hacerAlgo(){
+    // let i = 100;
+    // this.json.getData("").subscribe(respuesta => {
+    // this.json.getData("https://api.github.com/users/mhosan/repos").subscribe(respuesta => {
+    // console.log("Respuesta:");
+    // console.log(respuesta);
+      // console.log(respuesta.latt + ", " + respuesta.longt);
+      // tslint:disable-next-line:max-line-length
+      // console.log(respuesta[i].title + ", " + respuesta[i].year + ", " + respuesta[i].director + ", " + respuesta[i].cast + ", " + respuesta[i].genre);
+    // });
+  // }
+  hacerAlgo () {
     this.json.getData("http://api.population.io/1.0/population/World/today-and-tomorrow/?format=json").subscribe(respuesta => {
       console.log("Respuesta:", respuesta);
-      this.poblacion = {
+      this.pobMundial = {
         fechaHoy : respuesta.total_population[0].date,
         cantidadHoy : respuesta.total_population[0].population,
         fechaTomorrow: respuesta.total_population[1].date,
         cantidadTomorrow: respuesta.total_population[1].population
       };
     });
-  }
-  poblacionArgentina(event: any){
+  
     this.json.getData("http://api.population.io/1.0/population/Argentina/today-and-tomorrow/?format=json").subscribe(respuesta => {
       console.log("Respuesta:", respuesta);
-      this.poblacionArg = {
+      this.pobArgentina = {
         fechaHoy : respuesta.total_population[0].date,
         cantidadHoy : respuesta.total_population[0].population,
         fechaTomorrow: respuesta.total_population[1].date,
         cantidadTomorrow: respuesta.total_population[1].population
       };
     });
-  }
-  poblacionBrasil(event: any){
     this.json.getData("http://api.population.io/1.0/population/Brazil/today-and-tomorrow/?format=json").subscribe(respuesta => {
       console.log("Respuesta:", respuesta);
-      this.poblacionBr = {
+      this.pobBrasil = {
         fechaHoy : respuesta.total_population[0].date,
         cantidadHoy : respuesta.total_population[0].population,
         fechaTomorrow: respuesta.total_population[1].date,
         cantidadTomorrow: respuesta.total_population[1].population
       };
     });
-  }
-  poblacionChile(event: any){
     this.json.getData("http://api.population.io/1.0/population/Chile/today-and-tomorrow/?format=json").subscribe(respuesta => {
       console.log("Respuesta:", respuesta);
-      this.poblacionCh = {
+      this.pobChile = {
         fechaHoy : respuesta.total_population[0].date,
         cantidadHoy : respuesta.total_population[0].population,
         fechaTomorrow: respuesta.total_population[1].date,
         cantidadTomorrow: respuesta.total_population[1].population
       };
     });
-  }
-  poblacionBolivia(event: any){
     this.json.getData("http://api.population.io/1.0/population/Bolivia/today-and-tomorrow/?format=json").subscribe(respuesta => {
       console.log("Respuesta:", respuesta);
-      this.poblacionBl = {
+      this.pobBolivia  = {
         fechaHoy : respuesta.total_population[0].date,
         cantidadHoy : respuesta.total_population[0].population,
         fechaTomorrow: respuesta.total_population[1].date,
         cantidadTomorrow: respuesta.total_population[1].population
       };
     });
-  }
-  poblacionParaguay(event: any){
     this.json.getData("http://api.population.io/1.0/population/Paraguay/today-and-tomorrow/?format=json").subscribe(respuesta => {
       console.log("Respuesta:", respuesta);
-      this.poblacionPr = {
+      this.pobParaguay = {
         fechaHoy : respuesta.total_population[0].date,
         cantidadHoy : respuesta.total_population[0].population,
         fechaTomorrow: respuesta.total_population[1].date,
         cantidadTomorrow: respuesta.total_population[1].population
       };
     });
-  }
-  poblacionUruguay(event: any){
     this.json.getData("http://api.population.io/1.0/population/Uruguay/today-and-tomorrow/?format=json").subscribe(respuesta => {
       console.log("Respuesta:", respuesta);
-      this.poblacionUr = {
+      this.pobUruguay = {
         fechaHoy : respuesta.total_population[0].date,
         cantidadHoy : respuesta.total_population[0].population,
         fechaTomorrow: respuesta.total_population[1].date,
         cantidadTomorrow: respuesta.total_population[1].population
       };
     });
-  }
-  poblacionPeruana(event: any){
     this.json.getData("http://api.population.io/1.0/population/Peru/today-and-tomorrow/?format=json").subscribe(respuesta => {
       console.log("Respuesta:", respuesta);
-      this.poblacionPeru = {
+      this.pobPeru = {
         fechaHoy : respuesta.total_population[0].date,
         cantidadHoy : respuesta.total_population[0].population,
         fechaTomorrow: respuesta.total_population[1].date,
---
+++ src/app/poblacion.interface.ts
@@ -0,0 +1,6 @@
+export interface IPoblacion {
+    fechaHoy: string;
+    cantidadHoy: string;
+    fechaTomorrow: string;
+    cantidadTomorrow: string;
+}
